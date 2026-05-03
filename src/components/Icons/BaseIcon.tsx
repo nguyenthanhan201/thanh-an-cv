11e33542db4943a6748795193fe5cd6e21638b8f
An Nguyen <urbox@Frankenstein.local>
2026-05-03 11:11:40 +0700

    feat: add prisma

---
+++ src/components/Icons/BaseIcon.tsx
@@ -0,0 +1,6 @@
+// https://www.figma.com/design/mqEscnJbXzAB30GFddMDG9/Icon-Design-System---Material-Design-Icons--Community-?node-id=0-1&p=f&t=n80i7bEyeH6FbBA1-0
+const BaseIcon = () => {
+  return <div>BaseIcon</div>;
+};
+
+export default BaseIcon;
